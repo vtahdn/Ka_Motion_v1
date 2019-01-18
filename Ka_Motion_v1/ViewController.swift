89c30a458c1302d1fa851f00e46dabb9fc546dc8
Viet Asc <vtahdn@gmail.com>
2019-01-18 10:52:06 +0700

    Initial Commit

---
+++ Ka_Motion_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ka_Motion_v1
+//
+//  Created by Viet Asc on 1/18/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Ka_Motion_v1Tests/Ka_Motion_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Ka_Motion_v1Tests.swift
+//  Ka_Motion_v1Tests
+//
+//  Created by Viet Asc on 1/18/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import Ka_Motion_v1
+
+class Ka_Motion_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
